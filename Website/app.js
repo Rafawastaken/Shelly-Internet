94353f96f83f1066656c07ec98dcec80b8e42cad
Rafael Pimenta <jrafapimenta@gmail.com>
2022-01-26 22:38:59 +0000

    Done site design

---
+++ Website/app.js
@@ -0,0 +1,5 @@
+// Elementos Identificação
+
+const btn_ligar = document.getElementById("btn-ligar");
+const btn_desligar = document.getElementById("btn-desligar");
+const text_status = document.getElementById("span-status");
